15747a33aaac6d15f784ea3621a7ceb2d091ef8b
Lessllie Real <rlessllie@yahoo.com>
2019-08-22 23:58:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,45 @@
 # My Code here....
+
+def 
+  map_to_negativize(source_array)
+  array=[1,2,3,-9]
+  array.map {|n| n*-1}
+end 
+
+def 
+  map_to_no_change(source_array)
+  dune=["paul", "gurney", "vladimir", "jessica", "chani"]
+  
+end
+
+def 
+  map_to_double(source_array)
+  array=[1,2,3,-9]
+  array.map {|n| n*2}
+end 
+
+def
+map_to_square(source_array)
+array=[1,2,3,-9]
+array.map {|n|n**2 }
+end
+
+def
+reduce_to_total(array)
+array=[1,2,3]
+array.reduce {|sum|sum+num}
+end
+
+def
+reduce_to_all_true(source_array)
+array=[1,2,3]
+array.reduce(100) {|sum,n|sum+n}
+num=array[index]
+end 
+
+def
+reduce_to_any_true(source_array)
+ source_array = [ false, nil, nil, nil]
+my_list.reduce(false) { |memo, obj| memo && obj } 
+end 
+
