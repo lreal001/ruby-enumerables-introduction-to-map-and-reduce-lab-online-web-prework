87d68e8428ccdca266d6fd0d4ae2033b873e0e80
Lessllie Real <rlessllie@yahoo.com>
2019-08-23 00:52:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,60 @@
 # My Code here....
+
+def 
+  map_to_negativize(source_array)
+  array=[1,2,3,-9]
+  array.map {|n| n*-1}
+end 
+
+def 
+  map_to_no_change(source_array)
+  dune=["paul", "gurney", "vladimir", "jessica", "chani"]
+  
+end
+
+def 
+  map_to_double(source_array)
+  array=[1,2,3,-9]
+  array.map {|n| n*2}
+end 
+
+
+def
+map_to_square(source_array)
+array=[1,2,3,-9]
+array.map {|n|n**2 }
+end
+
+def
+reduce_to_total(array)
+array=[1,2,3]
+array.reduce (0, :+)
+end
+
+def
+reduce_to_all_true(source_array)
+array=[1,2,3]
+array.reduce(0, :+)
+num=array[index]
+end 
+
+def
+reduce_to_any_true(source_array)
+[ false, nil, nil, nil, true].any?
+end 
+
+def
+reduce_to_all_true(source_array)
+[1, 2, true, "razmatazz"].all?
+end
+
+def 
+  reduce_to_any_true(source_array)
+  [ false, nil, nil, nil, true].any?
+end 
+
+def 
+  reduce_to_any_true(source_array)
+  [ false, nil, nil, nil].any?
+end
+
